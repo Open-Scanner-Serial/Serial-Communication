9fde8ef2159853a36c7090f50dd6efb0bacf8e10
bermaj <bermaj@rpi.edu>
2019-04-13 18:21:35 -0400

    Added additional commands and helper functions

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import SerialPort from "serialport"
 
 export class WhistlerDevice {
 
-    private port: SerialPort;
+    private port?: SerialPort;
 
-    constructor(_port: string, _baudrate: number) {
-        this.port = new SerialPort(_port, {
-            baudRate: _baudrate
-        });
+    constructor(_port: string, _baudrate: number, debug: boolean) {
+        if (!debug) {
+
+            this.port = new SerialPort(_port, {
+                baudRate: _baudrate
+            });
+        }
     }
 
     private add(sum: number, num: number) {
@@ -23,7 +26,7 @@ export class WhistlerDevice {
         message[0] = 0x00;
         var i = 0;
 
-        for (i; i < command.length, i++) {
+        for (i; i < command.length; i++) {
             message[i+1] = command[i];
         }
 
@@ -33,22 +36,63 @@ export class WhistlerDevice {
         message[i] = this.calculateChecksum(message);
         i++;
         message[0] = 0x02;
-        this.port.write(Buffer.from(message.slice(0,i)));
+        //this.port.write(Buffer.from(message.slice(0,i)));
     }
 
     private readMessage() {
-        this.port.on('readable', () => {
-            console.log(this.port.read());
-        });
+        //this.port.on('readable', () => {
+        //    console.log(this.port.read());
+        //});
+    }
+
+    private charToByte(char: string) {
+        var bytes = char.charCodeAt(0).toString(16);
+        return bytes
+    }
+
+
+    public goMenu() {
+        this.writeMessage([0x4B, 17]);
+    }
+
+    public getStatus() {
+        this.writeMessage([0x41]);
+    }
+
+    public getLCD() {
+        this.writeMessage([0x4C]);
+    }
+
+    public getActiveChannelInformatino() {
+        this.writeMessage([0x61]);
+    }
+
+    public getPowerStatus() {
+        this.writeMessage([0x50]);
     }
 
     public getVersion() {
-        this.writeMessage([0x50]);
-        this.readMessage();
+        this.writeMessage([0x56, 0x00]);
     }
 
-    public goMenu() {
-        this.writeMessage([0x4B, 17])
+    private setCCDump(status: number) {
+        this.writeMessage([0x43, status]);
     }
+
+    public setCCDumpOn()  {
+        this.setCCDump(1);
+    }
+
+    public setCCDumpOff() {
+        this.setCCDump(0);
+    }
+
+
+
 }
+
+//var device = new WhistlerDevice('COM7', 15200, true);
+
+//console.log(device.charToByte('K'));
+
 
