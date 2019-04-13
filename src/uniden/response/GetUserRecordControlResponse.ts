018b10bf3c055415ce895eda6aed001fba73196a
bermaj <bermaj@rpi.edu>
2019-04-13 17:06:55 -0400

    Actually added user record control command

---
+++ src/uniden/response/GetUserRecordControlResponse.ts
@@ -0,0 +1,16 @@
+import { UnidenResponse } from "./UnidenResponse";
+
+export class GetUserRecordControlResponse extends UnidenResponse {
+
+  public isValid(): boolean {
+    if (this.rawValues.length < 3) return false;
+    return true;
+  }
+
+  public getVersion() {
+    return this.rawValues[2].trimRight();
+  }
+
+
+
+}
