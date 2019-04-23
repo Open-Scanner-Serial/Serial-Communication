d0beb1dc79b0589336bca5e2713d2d0d1693dc84
bermaj <bermaj@rpi.edu>
2019-04-23 01:46:42 -0400

    Added Implementations

--- src/uniden/response/GetSystemInfoResponse.ts
+++ src/uniden/response/GetSystemInfoResponse.ts
@@ -1,13 +1,13 @@
 import { UnidenResponse } from "./UnidenResponse";
 
-export class GetFirmwareVersionResponse extends UnidenResponse {
+export class GetSystemInfo extends UnidenResponse {
 
   public isValid(): boolean {
     if (this.rawValues.length < 3) return false;
     return true;
   }
 
-  public getVersion() {
-    return this.rawValues[2].trimRight();
+  public getSystemInfo() {
+    return this.rawValues;
   }
 }
